f0471067b4da8529141b45fd7784f00048d7bf7f
Seiei Miyagi <hanachin@gmail.com>
2019-07-08 00:06:29 +0900

    Set color palette <3

--- bootpack.c
+++ bootpack.c
@@ -1,15 +1,66 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette();
+void set_palette(int start, int end, unsigned char *rgb);
 
 void HariMain(void)
 {
-	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
+  init_palette();
 
-	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, i & 0xf); /* MOV BYTE [i],15 */
+  char *p = (char *)0xa0000;
+	for (int i = 0; i <= 0xffff; i++) {
+		p[i] = i & 0x0f;
 	}
 
   for(;;) {
     io_hlt();
   }
+}
+
+void init_palette() {
+  static unsigned char table_rgb[16*3] = {
+    0x00, 0x00, 0x00,
+    0xff, 0x00, 0x00,
+    0x00, 0xff, 0x00,
+    0xff, 0xff, 0x00,
+
+    0x00, 0x00, 0xff,
+    0xff, 0x00, 0xff,
+    0x00, 0xff, 0xff,
+    0xff, 0xff, 0xff,
+
+    0xc6, 0xc6, 0xc6,
+    0x84, 0x00, 0x00,
+    0x00, 0x84, 0x00,
+    0x84, 0x84, 0x00,
+
+    0x00, 0x00, 0x84,
+    0x84, 0x00, 0x84,
+    0x00, 0x84, 0x84,
+    0x84, 0x84, 0x84
+  };
+  set_palette(0, 15, table_rgb);
+  return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb) {
+  int eflags = io_load_eflags();
+  io_cli();
+  io_out8(0x03c8, start);
+  for (int i = start; i <= end; i++) {
+    // 4で割ってるのは
+    // > 書き込むときは上位2ビットを0に、読み込むときは上位2bitを0と見なす
+    // http://oswiki.osask.jp/?cmd=read&page=VGA&word=32%20bit
+    // これが理由
+    io_out8(0x03c9, rgb[0] / 4);
+    io_out8(0x03c9, rgb[1] / 4);
+    io_out8(0x03c9, rgb[2] / 4);
+    rgb += 3;
+  }
+  io_store_eflags(eflags);
+  return;
 }
--- naskfunc.nas
+++ naskfunc.nas
@@ -3,7 +3,17 @@ bits 32
 section .text
 
         GLOBAL io_hlt
-        GLOBAL write_mem8
+        GLOBAL io_cli
+        GLOBAL io_sti
+        GLOBAL io_stihlt
+        GLOBAL io_in8
+        GLOBAL io_in16
+        GLOBAL io_in32
+        GLOBAL io_out8
+        GLOBAL io_out16
+        GLOBAL io_out32
+        GLOBAL io_load_eflags
+        GLOBAL io_store_eflags
 
 write_mem8:	; void write_mem8(int addr, int data);
 		MOV		ECX,[ESP+4]		; [ESP+4]にaddrが入っているのでそれをECXに読み込む
@@ -13,4 +23,65 @@ write_mem8:	; void write_mem8(int addr, int data);
 
 io_hlt:
   HLT
+  RET
+
+io_cli:
+  CLI
+  RET
+
+io_sti:
+  STI
+  RET
+
+io_stihlt:
+  STI
+  RET
+
+io_in8:
+  MOV EAX, [ESP+4]
+  MOV EAX, 0
+  IN AL, DX
+  RET
+
+io_in16:
+  MOV EAX, [ESP+4]
+  MOV EAX, 0
+  IN AX, DX
+  RET
+
+io_in32:
+  MOV EAX, [ESP+4]
+  MOV EAX, 0
+  IN EAX, DX
+  RET
+
+io_out8:
+  MOV EDX, [ESP+4]
+  MOV AL, [ESP+8]
+  OUT DX,AL
+  RET
+
+
+io_out16:
+  MOV EDX, [ESP+4]
+  MOV AX, [ESP+8]
+  OUT DX,AX
+  RET
+
+
+io_out32:
+  MOV EDX, [ESP+4]
+  MOV EAX, [ESP+8]
+  OUT DX,EAX
+  RET
+
+io_load_eflags:
+  PUSHFD
+  POP EAX
+  RET
+
+io_store_eflags:
+  MOV EAX, [ESP+4]
+  PUSH EAX
+  POPFD
   RET
